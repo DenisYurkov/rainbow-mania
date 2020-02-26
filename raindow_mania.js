4b8c9669c4d241e6db439ead6c10811f3529a511
Denis Yurkov <denisdeveloper136@gmail.com>
2020-02-26 18:32:28 +0300

    Add File

---
+++ raindow_mania.js
@@ -0,0 +1,51 @@
+document.onkeydown = function (e) {
+    e = e || window.event;
+    
+    if (e.keyCode == 65) {
+        document.body.style.backgroundColor = "#a503fc";
+    };
+    
+    if (e.keyCode == 66) {
+        document.body.style.backgroundColor = "#fcba03";
+    };
+    
+    if (e.keyCode == 67) {
+        document.body.style.backgroundColor = "#a503fc";
+    };
+    
+    if (e.keyCode == 68) {
+        document.body.style.backgroundColor = "#fc036f";
+    };
+
+    if (e.keyCode == 69) {
+        document.body.style.backgroundColor = "#03fc45";
+    };
+
+    if (e.keyCode == 70) {
+        document.body.style.backgroundColor = "#fcca03";
+    };
+
+    if (e.keyCode == 71) {
+        document.body.style.backgroundColor = "#fc9d03";
+    };
+
+    if (e.keyCode == 72) {
+        document.body.style.backgroundColor = "#03c2fc";
+    };
+
+    if (e.keyCode == 73) {
+        document.body.style.backgroundColor = "#fc0303";
+    };
+    
+    if (e.keyCode == 74) {
+        document.body.style.backgroundColor = "#03fcf0";
+    };
+    
+    if (e.keyCode == 75) {
+        document.body.style.backgroundColor = "#0703fc";
+    };
+
+    if (e.keyCode == 76) {
+        document.body.style.backgroundColor = "#0743fc";
+    };
+};
